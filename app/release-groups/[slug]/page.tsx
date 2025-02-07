2dafae012c22c4081134bfc047af693811b89a66
Ash <ashley@juice49.com>
2025-02-07 21:59:57 +0000

    feat: release group detail page

--- app/release-groups/[slug]/page.tsx
+++ app/release-groups/[slug]/page.tsx
@@ -1,4 +1,13 @@
+import { notFound } from 'next/navigation'
 import type { ComponentType } from 'react'
+import { firstValueFrom } from 'rxjs'
+import { sanityClient } from '../../../sanity-client'
+
+const RELEASE_GROUP_QUERY = `
+  *[_type == "releaseGroup" && slug.current == $slug] {
+    name
+  }[0]
+`
 
 interface Props {
   params: Promise<{
@@ -8,9 +17,20 @@ interface Props {
 
 const Page: ComponentType<Props> = async ({ params }) => {
   const { slug } = await params
+
+  const releaseGroup = await firstValueFrom(
+    sanityClient.fetch(RELEASE_GROUP_QUERY, {
+      slug,
+    }),
+  )
+
+  if (releaseGroup === null) {
+    return notFound()
+  }
+
   return (
     <>
-      <h1>release-group: {slug}</h1>
+      <h1>{releaseGroup.name}</h1>
     </>
   )
 }
