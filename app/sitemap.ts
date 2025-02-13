916c3b67fe45d80cde478c7da828e867f60e5b34
Ash <ashley@juice49.com>
2025-02-13 23:49:32 +0000

    feat: add sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,33 @@
+import type { MetadataRoute } from 'next'
+import { firstValueFrom } from 'rxjs'
+import { sanityClient } from '../sanity-client'
+
+const BASE_URL = 'https://fridayclub.co'
+
+const SITEMAP_QUERY = `
+  [
+    {
+      "url": $baseUrl,
+      "lastModified": *[_type == "article"] | order(publishedAt desc)[0].publishedAt,
+      "changeFrequency": "weekly",
+      "priority": 1,
+    },
+    ...*[_type in ["artist", "track", "releaseGroup"]] {
+      "url": select(
+        _type == "artist" => array::join([$baseUrl, "artists", slug.current], "/"),
+        _type == "track" => array::join([$baseUrl, "tracks", slug.current], "/"),
+        _type == "releaseGroup" => array::join([$baseUrl, "release-groups", slug.current], "/"),
+      ),
+      "lastModified": coalesce(_updatedAt, _createdAt),
+      "changeFrequency": "monthly"
+    }[defined(url)]
+  ]
+`
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  return firstValueFrom(
+    sanityClient.fetch(SITEMAP_QUERY, {
+      baseUrl: BASE_URL,
+    }),
+  )
+}
