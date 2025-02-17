4e3bfd0231feba144abc4ae54cadee179f8926e0
Ash <ashley@juice49.com>
2025-02-17 00:02:48 +0000

    feat: add article pages to XML sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -12,8 +12,16 @@ const SITEMAP_QUERY = `
       "changeFrequency": "weekly",
       "priority": 1,
     },
-    ...*[_type in ["artist", "track", "releaseGroup"]] {
+    ...*[_type in ["article", "artist", "track", "releaseGroup"]] {
       "url": select(
+        _type == "article" => array::join(
+          [
+            $baseUrl,
+            array::join(string::split(publishedAt, "-")[0...2], "/"),
+            slug.current
+          ],
+          "/"
+        ),
         _type == "artist" => array::join([$baseUrl, "artists", slug.current], "/"),
         _type == "track" => array::join([$baseUrl, "tracks", slug.current], "/"),
         _type == "releaseGroup" => array::join([$baseUrl, "release-groups", slug.current], "/"),
